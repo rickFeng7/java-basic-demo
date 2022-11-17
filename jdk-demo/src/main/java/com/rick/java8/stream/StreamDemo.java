6a8b8ca9d63e543224f62a46ab46539a0293097f
fengrui03 <45378344+rickFeng7@users.noreply.github.com>
2022-11-17 14:53:11 +0800

    java8 stream

---
+++ jdk-demo/src/main/java/com/rick/java8/stream/StreamDemo.java
@@ -0,0 +1,40 @@
+package com.rick.java8.stream;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author fengrui03
+ * @date 2022/11/15
+ */
+public class StreamDemo {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void test() {
+        Person p1 = new Person().setName("张三").setAge(18);
+        Person p2 = new Person().setName("李四").setAge(19);
+        Person p3 = new Person().setName("张三").setAge(18);
+        List<Person> people = new ArrayList<>();
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        // 根据equals去重，需重写toString方法
+        List<Person> list = people.stream().distinct().collect(Collectors.toList());
+        System.out.println(list);
+    }
+
+    @Data
+    @Accessors(chain = true)
+    static class Person {
+        private String name;
+        private int age;
+
+    }
+}
