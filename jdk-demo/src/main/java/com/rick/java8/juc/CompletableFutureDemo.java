ba87671fa036f69131b887305d6c4ac379af67ba
fengrui03 <45378344+rickFeng7@users.noreply.github.com>
2022-11-24 19:22:02 +0800

    CompletableFuture optimize

--- jdk-demo/src/main/java/com/rick/java8/juc/CompletableFutureDemo.java
+++ jdk-demo/src/main/java/com/rick/java8/juc/CompletableFutureDemo.java
@@ -1,5 +1,8 @@
 package com.rick.java8.juc;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -9,67 +12,63 @@ import java.util.concurrent.Executors;
 /**
  * @author fengrui
  */
+@Slf4j
 public class CompletableFutureDemo {
 
-    public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(5);
-        // 1.结果处理
-        // test_whenComplete(threadPool);
-        // test_handle(threadPool);
-        // test_exceptionally(threadPool);
-
-        // 2.结果转换
-        // test_thenApply(threadPool);
-        // test_thenCompose(threadPool);
-
-        // 3.结果消费
-        // test_thenAccept(threadPool);
-
-        // 4.结果组合
-        // test_thenCombine(threadPool);
-
-        // 5.任务交互
-    }
-
+    // 1.结果处理
     // 入参：返回结果、异常，无返回值
-    private static void test_whenComplete(ExecutorService threadPool) {
-        CompletableFuture<Void> runAsync = CompletableFuture.runAsync(() -> System.out.println("123")).whenComplete((unused, throwable) -> System.out.println("456"));
+    @Test
+    public void test_whenComplete() {
+        CompletableFuture<Void> runAsync = CompletableFuture.runAsync(() -> log.info("test_whenComplete: {}", "123"));
+        CompletableFuture<Void> whenComplete = runAsync.whenComplete((unused, throwable) -> log.info("test_whenComplete: {}", "456"));
     }
 
     // 与complete区别：返回future & 可自定义返回future，与apply区别：可以处理异常
-    private static void test_handle(ExecutorService threadPool) {
+    @Test
+    public void test_handle() {
         CompletableFuture<String> supplyAsync = CompletableFuture.supplyAsync(() -> "123");
         CompletableFuture<Integer> handle = supplyAsync.handle((s, error) -> s.length());
-        System.out.println(handle.join());
+        log.info("test_handle: {}", handle.join());
     }
 
-    private static void test_exceptionally(ExecutorService threadPool) {
+    @Test
+    public void test_exceptionally() {
         CompletableFuture<String> exceptionally = CompletableFuture.supplyAsync(() -> 1 / 0 + "").exceptionally(Throwable::getMessage);
-        System.out.println(exceptionally.join());
+        log.info("test_exceptionally: {}", exceptionally.join());
     }
 
 
+    // 2.结果转换
     // 返回future
-    private static void test_thenApply(ExecutorService threadPool) {
+    @Test
+    public void test_thenApply() {
         CompletableFuture<String> thenApply = CompletableFuture.supplyAsync(() -> "123").thenApply(s -> s);
-        System.out.println(thenApply.join());
+        log.info("test_thenApply: {}", thenApply.join());
     }
+
 
     // thenApply转换的是泛型中的类型，相当于将CompletableFuture<T> 转换生成新的CompletableFuture<U>
     // thenCompose用来连接两个CompletableFuture，是生成一个新的CompletableFuture
-    private static void test_thenCompose(ExecutorService threadPool) {
+    @Test
+    public void test_thenCompose() {
         CompletableFuture<String> thenCompose = CompletableFuture.supplyAsync(() -> "123").thenCompose(s -> CompletableFuture.supplyAsync(() -> s + "456"));
-        System.out.println(thenCompose.join());
+        log.info("test_thenCompose: {}", thenCompose.join());
     }
 
 
+    // 3.结果消费
     // 无返回值，只是消费
-    private static void test_thenAccept(ExecutorService threadPool) {
-        CompletableFuture<Void> thenAccept = CompletableFuture.supplyAsync(() -> "123").thenAccept(System.out::println);
-        System.out.println(thenAccept.join());
+    @Test
+    public void test_thenAccept() {
+        CompletableFuture<Void> thenAccept = CompletableFuture.supplyAsync(() -> "123").thenAccept(log::info);
+        log.info("test_thenAccept: {}", thenAccept.join());
     }
 
-    private static void test_thenCombine(ExecutorService threadPool) {
+
+    // 4.结果组合
+    @Test
+    public void test_thenCombine() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
         List<Integer> list1 = new ArrayList<>();
         list1.add(1);
         list1.add(2);
@@ -84,6 +83,9 @@ public class CompletableFutureDemo {
                     return list;
                 });
         // join无需处理异常，get需手动处理异常
-        System.out.println(combine.join());
+        log.info("test_thenCombine: {}", combine.join());
     }
+
+
+    // 5.任务交互
 }
